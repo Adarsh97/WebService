46d0be087e3137a17fd6c9e5593795157283dfaa
Adarsh Sunilkumar <adarshsunilkumar55@gmail.com>
2018-06-05 00:55:17 +0530

    Create Person.java

---
+++ jdbctemplate/src/com.ms/Person.java
@@ -0,0 +1,61 @@
+
+package com.ms;
+
+public class Person {
+
+	private String name;
+	private int age;
+	private int id;
+	private int salary;
+	
+	public Person(int id,String name,int age,int salary)
+	{
+		this.id=id;
+		this.age=age;
+		this.name=name;
+		this.salary=salary;
+	}
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int  getId()
+	{
+		return id;
+	}
+	public int getSalary()
+	{
+		return salary;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setAge(int age)
+	{
+		this.age=age;
+	}
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public void setSalary(int salary)
+	{
+		this.salary=salary;
+	}
+	@Override
+	public String toString()
+	{
+		return "name: "+name+" age: "+age+" id: "+id+"salary: "+salary;
+	}
+	
+}
